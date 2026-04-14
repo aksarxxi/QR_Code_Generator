322304b5940794b88ea56d94e65763ddc8ebf0c3
aksarxxi <aksar8030@gmail.comm>
2026-04-14 23:52:55 +0530

    fixed error - Image quality improved

--- qr_code_gen.py
+++ qr_code_gen.py
@@ -6,10 +6,16 @@ qr = qrcode.QRCode(
     version=1,
     box_size=10,
     border=5
-) #converts the link into QRcode
+)
+
+qr.add_data(link)
+qr.make(fit=True)
+
+#Convert to image
+img = qr.make_image(fill_color = "black", back_color= "white")
 
 qr_name = input("Enter a name to save the QR : ")
 
-qr.save(qr_name + ".png")
+img.save(qr_name + ".png")
 
 print(f"Your QR code generated and saved as {qr_name}.png")
